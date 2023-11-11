b43d0e973cbaef72778468333d77fb15cba99a80
Ajay Wagh <54326598+Ajaywagh2298@users.noreply.github.com>
2023-11-11 22:47:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());*/
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://almabetter-book-my-show-frontend.vercel.app',
     credentials: true,
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": true
